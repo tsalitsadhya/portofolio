e79ca2d30d608666efd3d2e9e67a722a265f968a
tsalitsadhya <tsalitsatul.adhya@student.president.ac.id>
2025-07-09 22:03:44 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
+const img = document.getElementById('illustration');
+let direction = 1;
 
-console.log("Learnlytics loaded");
+setInterval(() => {
+  if (direction === 1) {
+    img.style.transform = 'translateY(-10px)';
+    direction = -1;
+  } else {
+    img.style.transform = 'translateY(0)';
+    direction = 1;
+  }
+}, 1000);
